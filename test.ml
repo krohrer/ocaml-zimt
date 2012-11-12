0647a9e0d542cd0730c605e557131708df500ff1
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2012-11-12 18:30:16 +0100

    Test.

--- test.ml
+++ test.ml
@@ -4,12 +4,12 @@ open ObjC
 let classes = 
   let n = ref 0 in
   let pa = copy_class_list n in
-  let a = Array.make !n C.null in
+  let a = Array.make !n "" in
   for i = 0 to !n - 1 do
-    a.(i) <- C.ptr_array_ith pa i
+    a.(i) <- C.copy_cstr (Class.get_name (C.ptr_array_ith pa i))
   done;
   C.free pa;
   a
 ;;
 
-C.dump_cstr (Class.get_name classes.(0));;
+Array.iter print_endline classes
