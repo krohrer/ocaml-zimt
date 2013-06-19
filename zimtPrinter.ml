b14d213f678369cfa7379c14edca2a07ff0cbc11
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2013-06-19 23:26:54 +0200

    ZimtPrinter bare bones.

--- zimtPrinter.ml
+++ zimtPrinter.ml
@@ -1,7 +1,44 @@
+open PrettyPrinter
 type pp = PrettyPrinter.t
 
-let dump_expr = function
-  | _ -> failwith "TODO"
+open Zimt
+
+let rec dump_t : type t . t Zimt.t -> pp = function
+  | TCaml ml		-> dump_caml_t ml
+  | TForward (lazy t')	-> dump_forward t'
+  | TNamed (t',id)	-> dump_named t' id
+  | TPtr p		-> dump_ptr p
+  | TStruct s		-> dump_struct' s
+  | TEnum e		-> dump_enum e
+  | TPrim prim		-> dump_prim prim
+
+and dump_caml_t : type t . t Caml.t -> pp = fun ml ->
+  pp_string "TODO"
+
+and dump_forward : type t . t Zimt.t -> pp = fun t' ->
+  pp_string "TODO"
+
+and dump_named : type t . t Zimt.t -> q_ident -> pp = fun t' id ->
+  pp_string "TODO"
+
+and dump_ptr : type t . t Zimt.ptr -> pp = function
+  | _ -> pp_string "TODO"
+
+and dump_struct' : type t . t Zimt.struct' -> pp =
+  function
+  | _ -> pp_string "TODO"
+
+and dump_enum : type t . t Zimt.enum -> pp =
+  function
+  | _ -> pp_string "TODO"
+
+and dump_prim : type t . t Zimt.prim -> pp =
+  function
+  | _ -> pp_string "TODO"
+
+
+(* and dump_forward : type t . t Zimt.t Lazy.t = fun (lazy t') -> *)
+(*   pp_string "" *)
 
 let dump_type = function
   | _ -> failwith "TODO"
--- zimtPrinter.mli
+++ zimtPrinter.mli
@@ -1,5 +1,23 @@
 type pp = PrettyPrinter.t
 
-val dump_expr : 'a Zimt.x -> pp
-val dump_type : 'a Zimt.t -> pp
+(* val dump_t	: 'a Zimt.t		-> pp *)
+(* val dump_x	: 'a Zimt.x	-> pp *)
 
+(* val dump_forward	: 'a . 'a Zimt.t Lazy.t			-> pp *)
+(* val dump_named		: 'a . 'a Zimt.t -> Zimt.q_ident	-> pp *)
+(* val dump_ptr	        : 'a . 'a Zimt.ptr			-> pp *)
+(* val dump_struct' 	: 'a . 'a Zimt.struct'			-> pp *)
+(* val dump_enum		: 'a . 'a Zimt.enum			-> pp *)
+(* val dump_prim		: 'a . 'a Zimt.prim			-> pp   *)
+  
+(* val dump_ptr		: 'a . 'a Zimt.ptr			-> pp *)
+(* val dump_struct'	: 'a . 'a Zimt.struct'			-> pp *)
+(* val dump_enum		: 'a . 'a Zimt.enum			-> pp *)
+(* val dump_field		: 'a 'b . ('a,'b) Zimt.field		-> pp *)
+(* val dump_prim		: 'a . 'a Zimt.prim			-> pp *)
+(* val dump_fn		: 'a . 'a Zimt.fn			-> pp *)
+(* val dump_varargs	: Zimt.varargs				-> pp *)
+
+(* val dump_lit		: 'a . 'a Zimt.lit			-> pp *)
+(* val dump_op1		: 'a 'b . ('a,'b) Zimt.op1		-> pp *)
+(* val dump_op2		: 'a 'b 'c . ('a,'b,'c) Zimt.op2	-> pp *)
